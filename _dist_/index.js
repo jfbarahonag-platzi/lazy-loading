1bb351d8580474e9754e2cb4b16cf6927438a385
jfbarahonag <jfbarahonag@users.noreply.github.com>
2021-04-14 06:49:39 +0000

    deploy: 7c56cf2556a8453be6ca5a7b9cfadf9d61b5be1e

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,51 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+function createElement(type, className='') {
+    const node = document.createElement(type)
+    if (className != '') {
+        node.className = className
+    }
+    return node
+}
+
+function addImage(src, width) {
+    let images = createElement('div')
+    images.id = 'images'
+    //
+    let div = createElement('div', 'p-4')
+    //
+    const element = createElement('img', 'mx-auto rounded-lg')
+    element.width = width
+    element.src = src
+    div.append(element)
+    images.append(div)
+    //console.log(images);
+    return images
+}
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const parent = document.querySelector('.text-center')
+
+let printImage = () => {
+    return addImage(`https://randomfox.ca/images/${generateRandomNumber(1, 122)}.jpg`, 320)
+}
+
+function printRandomImages (qty) {
+    const imagesArray = []
+    for (let index = 0; index < qty; index++) {
+        imagesArray.push(printImage())
+    }
+    console.log(printImage());
+    parent.append(...imagesArray)
+
+}
+
+printRandomImages(5)     
+
+
+
+
