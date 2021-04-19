b747acf16820912e3dbe535ef44321c25543cfcc
jfbarahonag <jfbarahonag@unal.edu.co>
2021-04-19 00:24:19 -0500

    lazy loading is full implemented, actions with buttons added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+/**
+ * import libraries
+ */
 import { registerImage } from "./lazy";
 
+/**
+ * Elements
+ */
 const addButton = document.querySelector('button')
+const cleanButton = document.querySelector('.clean')
+const removeFirstButton = document.querySelector('.remove')
 const parent = document.querySelector('#images')
 
+
+/**
+ * First level functions
+ */
 function createElement(type, className='') {
     const node = document.createElement(type)
     if (className != '') {
@@ -11,6 +23,22 @@ function createElement(type, className='') {
     return node
 }
 
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const checkIfChildExists = (parent) => {
+    const length = parent.childNodes.length
+    return length
+}
+
+const removeFirstChild = (parent) => {
+    parent.removeChild(parent.firstChild)
+}
+
+/**
+ * Second level functions
+ */
 function generateRandomImage(src, width) {
     let container = createElement('div', 'p-2 bg-yellow-700 bg-opacity-50 m-1 rounded-lg')
     //
@@ -23,30 +51,64 @@ function generateRandomImage(src, width) {
     return container
 }
 
-function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-let printImage = () => {
+let generateImage = () => {
     return generateRandomImage(`https://randomfox.ca/images/${generateRandomNumber(1, 122)}.jpg`, 320)
 }
 
 function printRandomImages (qty, container) {
     const imagesArray = []
     for (let index = 0; index < qty; index++) {
-        let image = printImage()
+        let image = generateImage()
         imagesArray.push(image)
         registerImage(image)
     }
-    //console.log(printImage());
+    //console.log(generateImage());
     container.append(...imagesArray)   
 }
 
+const isImageAvailable = () => {
+    return checkIfChildExists(parent)
+}
+
+const removeImage = () => {
+    removeFirstChild(parent)
+}
+
+/**
+ * Callbacks
+ */
 const addImage = () => {
     printRandomImages(1, parent)
 }
 
+const removeAllImages = () => {
+    
+    if (!isImageAvailable()) {
+        alert("Oops")
+        return
+    }
+
+    while (parent.firstChild != null) {
+        removeImage()
+    }
+}
+
+const removeFirstImage = () => {
+    
+    if (!isImageAvailable()) {
+        alert("Oops")
+        return
+    }
+
+    removeImage()
+}
+
+/**
+ * Event listeners
+ */
 addButton.addEventListener('click', addImage)     
+cleanButton.addEventListener('click', removeAllImages)     
+removeFirstButton.addEventListener('click', removeFirstImage)     
 
 
 
